3a4cc9f88d5abb9b6a6ec2dde0deac4647b3b1d8
nishino0330 <nishino220092@eggnet.ac.jp>
2023-10-18 12:16:05 +0900

    画像、材料を表示

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,10 +5,12 @@
 </head>
 <body>
     <h1>Recipe Search</h1>
-
     <form action="/search" method="GET">
         <label for="ingredients">Ingredients:</label>
         <input type="text" name="ingredients" id="ingredients">
+        
+        <input type="checkbox" id="japanese" name="japanese" />
+        <label for="japanese">japanese</label>
         <button type="submit">Search</button>
     </form>
 
@@ -16,16 +18,15 @@
         <h2>Search Results:</h2>
         @if(count($recipes) > 0)
             <ul>
-            @foreach($recipes as $recipe)
+                @foreach($recipes as $recipe)
+                    <!--料理名と画像を表示 -->
                     <li>{{ $recipe['recipe']['label'] }}</li>
-                    <img src="{{ $recipe['recipe']['image'] }}">
-                    
-                    <!--
-                    <li>{{ $recipe['recipe']['ingredientLines'][0] }}</li>
-                    <li>{{ $recipe['recipe']['ingredientLines'][1] }}</li>
-                    <li>{{ $recipe['recipe']['ingredientLines'][2] }}</li>
-                    -->
-                    
+                    <img src="{{ $recipe['recipe']['image'] }}" alt="{{ $recipe['recipe']['label'] }}">
+
+                    <!-- 料理の作り方を表示 -->
+                    @foreach($recipe['recipe']['ingredientLines'] as $step)
+                        <li>{{ $step }}</li>
+                    @endforeach
                 @endforeach
             </ul>
         @else
